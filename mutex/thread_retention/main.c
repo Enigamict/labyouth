bb2d3d1c065c9c2ebd1a1214b4a39a5077f25fb2
Ubuntu <mochienper@gmail.com>
2022-09-06 13:17:02 +0900

    mutex

--- mutex/thread_retention/main.c
+++ mutex/thread_retention/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 int thread_count = 0;
@@ -11,16 +12,34 @@ int thread_index = 0;
 int thread_loop[100];
 
 void *ret1() {
+//    pthread_mutex_lock(&m);
+//    int thread_loop_cnt = thread_loop[thread_index];
+//    printf("%d::%d\n", thread_index, thread_loop[thread_index]);
+//
+//
+//    for (int i = 0; i < thread_loop_cnt; i++) {
+//        thread_count++;
+//    }
+//
+//    for (int i = 0; i < thread_loop_cnt; i++){
+//        thread_count--;
+//    }
+//    printf("%d\n", thread_count);
+//    pthread_mutex_unlock(&m);
 
     pthread_mutex_lock(&m);
     int thread_loop_cnt = thread_loop[thread_index];
     printf("%d::%d\n", thread_index, thread_loop[thread_index]);
+    pthread_mutex_unlock(&m);
 
 
+    pthread_mutex_lock(&m);
     for (int i = 0; i < thread_loop_cnt; i++) {
         thread_count++;
     }
+    pthread_mutex_unlock(&m);
 
+    pthread_mutex_lock(&m);
     for (int i = 0; i < thread_loop_cnt; i++){
         thread_count--;
     }
@@ -34,6 +53,7 @@ int main() {
 
    pthread_t thread[100];
    const int main_loop = 100;
+   clock_t begin = clock();
 
    for (int i = 0; i < main_loop; i++) {
         pthread_mutex_lock(&m);
@@ -56,5 +76,9 @@ int main() {
 
 
    printf("thread_cnt:%d\n", thread_count);
+   clock_t end = clock();
+
+   printf("result: %f seconds\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
    pthread_mutex_destroy(&m);
 }
