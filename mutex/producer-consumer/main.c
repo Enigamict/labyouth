4e57fec41062c6c69f72f9982a61186722afa2c6
Ubuntu <mochienper@gmail.com>
2022-10-27 14:39:02 +0900

    producer-consumer

--- mutex/producer-consumer/main.c
+++ mutex/producer-consumer/main.c
@@ -12,42 +12,47 @@ pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 queue *que;
 
-void* producer() {
+void *thread_push() {
 
     pthread_mutex_lock(&mut);
     push(que, 1);
     push(que, 2);
+    push(que, 3);
+    push(que, 4);
+    push(que, 5);
     printf("push\n");
     pthread_cond_broadcast(&cond);
     pthread_mutex_unlock(&mut);
     return NULL;
 }
 
-void* consumer() { 
+void *thread_pop() { 
     pthread_mutex_lock(&mut);
 
     if (isEmpty(que)) {
-        pthread_mutex_unlock(&mut);
-        pthread_cond_wait(&cond, &mut);
+        pthread_mutex_unlock(&mut); // lockをかける理由としてifまでの間にメソッドが入らないようにするため
+        pthread_cond_wait(&cond, &mut); // 解放した後はまたロック
     }
 
     pop(que);
+    pthread_mutex_unlock(&mut); // unlock 
     return NULL;
 }
 
 int main() {
 
-    pthread_t th1, th2;
+    pthread_t th1, th2, th3;
 
     que = newQueue();
 
-    pthread_create(&th2, NULL, consumer, NULL);
 
-    sleep(5);
-    pthread_create(&th1, NULL, producer, NULL);
+    pthread_create(&th1, NULL, thread_push, NULL);
+    pthread_create(&th2, NULL, thread_pop, NULL);
+    pthread_create(&th3, NULL, thread_pop, NULL);
 
     pthread_join(th1, NULL); 
     pthread_join(th2, NULL);
+    pthread_join(th3, NULL);
 
     pthread_mutex_destroy(&mut);
 
